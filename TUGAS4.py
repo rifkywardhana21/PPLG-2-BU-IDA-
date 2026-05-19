5009573c090b4a6d1a4615ae02ba7192ef402886
Muhammad Rifky Wardhana <152211890+rifkywardhana21@users.noreply.github.com>
2026-05-19 09:50:46 +0700

    Add files via upload

---
+++ TUGAS4.py
@@ -0,0 +1,14 @@
+print("=== PROGRAM LOOPING KONTROL ===")
+
+for i in range(1, 11):
+
+    if i == ____:
+        _______
+
+    if i == ____:
+        print("Angka 8 ditemukan, looping dihentikan")
+        _______
+
+    print("Angka ke-", i)
+
+print("Looping selesai")
