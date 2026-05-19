58fa04f3e836735062274e732c6560c0ea98a6dd
Muhammad Rifky Wardhana <152211890+rifkywardhana21@users.noreply.github.com>
2026-05-19 09:49:23 +0700

    Add files via upload

---
+++ TUGAS-2.py
@@ -0,0 +1,12 @@
+total = 0
+
+print("=== PROGRAM HITUNG NILAI ===")
+
+for i in range(1, 6):
+    nilai = ________(input("Masukkan nilai ke-{i}: "))
+    total ______ nilai
+
+rata_rata = total / ____
+
+print("Total nilai :", total)
+print("Rata-rata nilai :", rata_rata)
